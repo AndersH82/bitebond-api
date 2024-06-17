a836a370331e2397b543204b219293467caaae03
AndersH82 <andersthereseholst@gmail.com>
2024-06-17 11:46:27 +0200

    add fields to recipes

--- api/models.py
+++ api/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', blank=True)
+    name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
     bio = models.TextField(blank=True)
 
     def __str__(self):
@@ -12,7 +15,12 @@ class Profile(models.Model):
 
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
+    ingredients = models.TextField(null=True, blank=True)
     description = models.TextField()
+    prep_time = models.IntegerField(null=True, blank=True)
+    cook_time = models.IntegerField(null=True, blank=True)
+    servings = models.IntegerField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
