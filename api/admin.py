10610009fe1f312e4c19592bbd7e5bb0ee0c04df
AndersH82 <andersthereseholst@gmail.com>
2024-06-17 11:37:04 +0200

    add views to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Profile, Recipe, Comment
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'recipe', 'created_at')
